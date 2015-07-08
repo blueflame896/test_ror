94fc26d75f6e4302f7e9c7bec7e4a25e33cf8a6a
Ahsan Ellahi <blueflame896@gmail.com>
2015-07-08 10:42:26 +0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitProjectTest::Application.config.secret_token = '9a1cd0eceaab50c7a34611e86067a1da9d734f9f7b5e015abf9e0705b66c12e0e6c3f16d6bbd3f673307bce354f44ffc91e841369fc960c750c9eeeb22c60e04'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GitProjectTest::Application.config.session_store :cookie_store, key: '_GitProjectTest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GitProjectTest::Application.config.session_store :active_record_store
